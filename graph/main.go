3ab59fa3ffb3be9889280633436ab8cd84512af8
emohankrishna <edemohankrishna1995@gmail.com>
2023-04-15 15:18:32 +0530

    Added Graph implementation

--- graph/main.go
+++ graph/main.go
@@ -1,59 +1,193 @@
 package main
 
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+)
 
 type Graph struct {
-	vertices []*Vertex
-	memo     map[int]int
+	vertexMap map[int][]*Node
 }
 
-type Vertex struct {
-	key       int
-	adjacency []*Vertex
-}
-
-func (g *Graph) addVertex(k int) {
-	previousLength := len(g.vertices)
-	g.memo[k] = previousLength
-	g.vertices = append(g.vertices, &Vertex{key: k, adjacency: []*Vertex{}})
-}
-
-func (g *Graph) addEdge(from int, to int) {
-	fromVertex := g.getVertex(from)
-	toVertex := g.getVertex(to)
-	if fromVertex == nil || toVertex == nil {
-		err := fmt.Errorf("Invalid Edge %d --> %d", from, to)
-		fmt.Println(err.Error())
-		return
+func NewGraph() *Graph {
+	return &Graph{
+		vertexMap: make(map[int][]*Node),
 	}
-	fromVertex.adjacency = append(fromVertex.adjacency, toVertex)
-	toVertex.adjacency = append(toVertex.adjacency, fromVertex)
 }
-func (g *Graph) getVertex(v int) *Vertex {
-	if i, ok := g.memo[v]; ok {
-		return g.vertices[i]
+
+type Node struct {
+	key    int
+	weight int
+}
+
+func (n Node) String() string {
+	return fmt.Sprintf("%d", n.key)
+}
+
+func (g *Graph) addVertex(v int) {
+	g.vertexMap[v] = []*Node{}
+}
+func getNode(k int) *Node {
+	return &Node{
+		key: k,
 	}
-	return nil
 }
-func (g *Graph) printGraph() {
-	for _, v := range g.vertices {
-		fmt.Printf("\nVertex : %d with edges ", v.key)
-		for _, v := range v.adjacency {
-			fmt.Printf(" %d ", v.key)
+
+func (g *Graph) isEdgeExist(s, d int) bool {
+	list, ok := g.vertexMap[s]
+	if ok {
+		for _, v := range list {
+			if v.key == d {
+				return true
+			}
 		}
 	}
+
+	return false
 }
 
-func main() {
-	test := &Graph{memo: make(map[int]int), vertices: []*Vertex{}}
-	for i := 0; i < 5; i++ {
-		test.addVertex(i)
+func (g *Graph) removeVertex(v int) {
+	list := g.vertexMap[v]
+	for _, node := range list {
+		l := g.vertexMap[node.key]
+		if index := getIndex(l, v); index != -1 {
+			g.vertexMap[node.key] = removeAtIndex(l, index)
+		}
 	}
-	test.addEdge(0, 2)
-	test.addEdge(1, 2)
-	test.addEdge(2, 3)
-	test.addEdge(4, 3)
-	test.addEdge(4, 2)
-	test.printGraph()
+	delete(g.vertexMap, v)
+}
+
+func (g *Graph) addEdge(s, d int) {
+	sNode := getNode(s)
+	dNode := getNode(d)
+	if !g.isEdgeExist(s, d) {
+		g.vertexMap[s] = append(g.vertexMap[s], dNode)
+	}
+	if !g.isEdgeExist(d, s) {
+		g.vertexMap[d] = append(g.vertexMap[d], sNode)
+	}
+
+}
+
+func getIndex(list []*Node, x int) int {
+	for i, node := range list {
+		if node.key == x {
+			return i
+		}
+	}
+	return -1
+}
+func removeAtIndex(slice []*Node, s int) []*Node {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func (g *Graph) removeEdge(s, d int) {
+	slist := g.vertexMap[s]
+	dlist := g.vertexMap[d]
+	if i := getIndex(slist, d); i != -1 {
+		g.vertexMap[s] = removeAtIndex(slist, i)
+	}
+	if i := getIndex(dlist, s); i != -1 {
+		g.vertexMap[d] = removeAtIndex(dlist, i)
+	}
+}
+
+func (g *Graph) printGraph() {
+	for key, node := range g.vertexMap {
+		fmt.Printf("%d --> %v\n", key, node)
+	}
+}
+
+func (g *Graph) ConnectedComponents() int {
+	visited := make(map[int]bool, len(g.vertexMap))
+	q := list.New()
+	count := 0
+	for k, _ := range g.vertexMap {
+		if !visited[k] {
+			q.PushBack(k)
+			visited[k] = true
+			for q.Len() > 0 {
+				e := q.Front()
+				q.Remove(e)
+				adjList := g.vertexMap[e.Value.(int)]
+				for _, n := range adjList {
+					if !visited[n.key] {
+						visited[n.key] = true
+						q.PushBack(n.key)
+					}
+				}
+
+			}
+			count++
+		}
+	}
+	return count
+}
+
+func (g *Graph) BFS(v int) []int {
+	visited := map[int]bool{}
+	bfs := []int{}
+	queue := list.New()
+	queue.PushBack(v)
+	for queue.Len() > 0 {
+		element := queue.Front()
+		queue.Remove(element)
+		vList, ok := g.vertexMap[element.Value.(int)]
+		if ok {
+			for _, val := range vList {
+				if !visited[val.key] {
+					visited[val.key] = true
+					bfs = append(bfs, val.key)
+					queue.PushBack(val.key)
+				}
+			}
+		}
+	}
+	return bfs
+}
+
+func (g *Graph) DFS(v int) []int {
+	dfs := []int{}
+	visited := map[int]bool{}
+	stack := list.New()
+	stack.PushBack(v)
+	for stack.Len() > 0 {
+		element := stack.Back()
+		stack.Remove(element)
+		vList, ok := g.vertexMap[element.Value.(int)]
+		if ok {
+			for _, val := range vList {
+				if !visited[val.key] {
+					visited[val.key] = true
+					dfs = append(dfs, val.key)
+					stack.PushBack(val.key)
+				}
+			}
+		}
+	}
+	return dfs
+}
+func main() {
+
+	graph := NewGraph()
+	graph.addVertex(1)
+	graph.addVertex(2)
+	graph.addEdge(1, 2)
+	graph.addEdge(2, 3)
+	graph.addEdge(2, 4)
+	graph.addEdge(3, 4)
+	graph.addEdge(5, 1)
+	graph.addEdge(2, 5)
+	graph.addEdge(6, 4)
+	graph.addEdge(7, 9)
+	graph.addEdge(8, 9)
+	graph.addEdge(2, 10)
+	graph.addEdge(1, 9)
+	graph.addVertex(11)
+	graph.addVertex(12)
+	graph.printGraph()
+	graph.BFS(1)
+	graph.DFS(1)
+	graph.ConnectedComponents()
 
 }
